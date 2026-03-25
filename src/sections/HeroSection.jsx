82aa145ab138949e1b61a51c40399e4e81ebcbc8
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2026-03-25 23:54:45 +0330

    Implement Text Animation Of( HeroSection )

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,4 +1,42 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+
 const HeroSection = () => {
+	useGSAP(() => {
+		const titleSplit = SplitText.create(".hero-title", {
+			type: "chars",
+		});
+
+		const tl = gsap.timeline({
+			delay: 1,
+		});
+        tl
+            .to(".hero-content", {
+			opacity: 1,
+			y: 0,
+			ease: "power1.inOut",
+		})
+			.to(
+				".hero-text-scroll",
+				{
+					duration: 1.2,
+					clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+					ease: "circ.out",
+				},
+				"-=0.5"
+			)
+			.from(
+				titleSplit.chars,
+				{
+					yPercent: 200,
+					stagger: 0.02,
+					ease: "power2.out",
+				},
+				"-=0.5"
+			);
+	});
+
 	return (
 		<section className="bg-main-bg">
 			<div className="hero-container">
@@ -7,7 +45,7 @@ const HeroSection = () => {
 					alt="hero-img"
 					className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-[1.5]"
 				/>
-				<div className="hero-content">
+				<div className="hero-content opacity-0">
 					<div className="overflow-hidden">
 						<h1 className="hero-title">Fucking Delicious</h1>
 					</div>
