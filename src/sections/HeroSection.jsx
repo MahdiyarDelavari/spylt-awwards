3e658de60841fc492f2d76634b444f904c17e9da
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2026-03-26 21:23:46 +0330

    Implement Video-Animation Of ( HeroSection )

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,8 +1,17 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
+import { useMediaQuery } from "react-responsive";
 
 const HeroSection = () => {
+	const isMobile = useMediaQuery({
+		query: "(max-width: 768px)",
+	});
+
+	const isTablet = useMediaQuery({
+		query: "(max-width: 1024px)",
+	});
+
 	useGSAP(() => {
 		const titleSplit = SplitText.create(".hero-title", {
 			type: "chars",
@@ -11,8 +20,8 @@ const HeroSection = () => {
 		const tl = gsap.timeline({
 			delay: 1,
 		});
-        tl
-            .to(".hero-content", {
+
+		tl.to(".hero-content", {
 			opacity: 1,
 			y: 0,
 			ease: "power1.inOut",
@@ -20,8 +29,8 @@ const HeroSection = () => {
 			.to(
 				".hero-text-scroll",
 				{
-					duration: 1.2,
-					clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+					duration: 1,
+					clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
 					ease: "circ.out",
 				},
 				"-=0.5"
@@ -34,55 +43,71 @@ const HeroSection = () => {
 					ease: "power2.out",
 				},
 				"-=0.5"
-        );
-        
+			);
 
-        const heroTl = gsap.timeline({
-            scrollTrigger: {
-                trigger: ".hero-container",
-                start: "1% top",
-                end: "bottom top",
-                scrub: true,
-            }
-        });
-        heroTl.to(".hero-container", {
-            rotate: 7,
-            scale: 0.9,
-            yPercent: 30,
-            ease:"power2.inOut"
-        })
+		const heroTl = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".hero-container",
+				start: "1% top",
+				end: "bottom top",
+				scrub: true,
+			},
+		});
+		heroTl.to(".hero-container", {
+			rotate: 7,
+			scale: 0.9,
+			yPercent: 30,
+			ease: "power1.inOut",
+		});
 	});
 
 	return (
 		<section className="bg-main-bg">
 			<div className="hero-container">
-				<img
-					src="/images/static-img.png"
-					alt="hero-img"
-					className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-[1.5]"
-				/>
+				{isTablet ? (
+					<>
+						{isMobile && (
+							<img
+								src="/images/hero-bg.png"
+								className="absolute bottom-40 size-full object-cover"
+							/>
+						)}
+						<img
+							src="/images/hero-img.png"
+							className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto"
+						/>
+					</>
+				) : (
+					<video
+						src="/videos/hero-bg.mp4"
+						autoPlay
+						muted
+						playsInline
+						className="absolute inset-0 w-full h-full object-cover"
+					/>
+				)}
 				<div className="hero-content opacity-0">
 					<div className="overflow-hidden">
-						<h1 className="hero-title">Fucking Delicious</h1>
+						<h1 className="hero-title">Freaking Delicious</h1>
 					</div>
 					<div
-						className="hero-text-scroll"
 						style={{
 							clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
 						}}
+						className="hero-text-scroll"
 					>
 						<div className="hero-subtitle">
-							<h1>Protein + Caffine</h1>
+							<h1>Protein + Caffine </h1>
 						</div>
 					</div>
 
 					<h2>
-						Live Life To The Fullest With SPYLT: Shatter Boredom And Embrace
-						Your Inner Kid With Every Delicously Smooth Chug
+						Live life to the fullest  with SPYLT: Shatter boredom and embrace
+						your inner kid with every deliciously smooth chug.
 					</h2>
 
 					<div className="hero-button">
-						<p>Chug a spylt</p>
+						<p>Chug a SPYLT</p>
 					</div>
 				</div>
 			</div>
