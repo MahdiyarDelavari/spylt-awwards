7ff2e09e9ed2ee8cdeee229824ad1e0c92c3e0a4
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2026-03-26 17:29:08 +0330

    Implement ( FlavorSection )

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import gsap from "gsap"
-import Navbar from "./components/Navbar"
-import HeroSection from "./sections/HeroSection"
-import { ScrollTrigger } from "gsap/all"
+import gsap from "gsap";
+import Navbar from "./components/Navbar";
+import HeroSection from "./sections/HeroSection";
+import { ScrollTrigger } from "gsap/all";
 import MessageSection from "./sections/MessageSection";
+import FlavorSection from "./sections/FlavorSection";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
-  return (
-    <main>
-      <Navbar />
-      <HeroSection />
-      <MessageSection/>
-      <div className="h-dvh border-4 border-emerald-800"></div>
-    </main>
-  )
-}
+	return (
+		<main>
+			<Navbar />
+			<HeroSection />
+			<MessageSection />
+			<FlavorSection />
+			<div className="h-dvh border-4 border-emerald-800"></div>
+		</main>
+	);
+};
 
-export default App
+export default App;
